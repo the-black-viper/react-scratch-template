46376b7433239284ec9d5a28492f2b490085420a
Joshua Javier <joshua.javier.github@gmail.com>
2021-08-05 11:55:49 +0800

    Working demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from react
-import ReactDom from react-dom
-const root = document.getElementById('root')
+import React from "react";
+import ReactDom from "react-dom";
+
+const root = document.getElementById("root");
 const App = () => {
-return (
-<h1>Hello from React</h1>
-)
-}
-ReactDom.render(<App />, root)
+  return <h1>Hello from React</h1>;
+};
+ReactDom.render(<App />, root);
